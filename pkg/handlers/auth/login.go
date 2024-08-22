fcee13d1e1ff4e0a99af74bed3893e7dc1c83d12
polivera <punk.moon2229@fastmail.com>
2024-08-23 01:30:42 +0200

    Start using htmx for the login form

--- pkg/handlers/auth/login.go
+++ pkg/handlers/auth/login.go
@@ -2,10 +2,9 @@ package auth
 
 import (
 	"context"
-	"net/http"
-
 	"homecomp/pkg/configs"
 	"homecomp/pkg/templates"
+	"net/http"
 )
 
 type LoginHandler interface {
@@ -28,9 +27,14 @@ func NewLoginHandler(cnf configs.Config, ctx context.Context) LoginHandler {
 // Login implements LoginHandler.
 func (l *loginHaddler) Handle(mux *http.ServeMux) {
 	mux.HandleFunc("GET /login", l.showLoginForm)
+	mux.HandleFunc("POST /login", l.loginSubmit)
 }
 
 func (l *loginHaddler) showLoginForm(w http.ResponseWriter, _ *http.Request) {
 	component := templates.LoginPage(l.conf)
 	component.Render(l.ctx, w)
+}
+
+func (l *loginHaddler) loginSubmit(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("garompeta"))
 }
